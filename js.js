21901e33838ca28a94de54fdf2cb57127d6cb8f7
Termisfa <chipichop12@hotmail.com>
2020-06-14 18:46:53 +0200

    Update js.js

--- js.js
+++ js.js
@@ -215,7 +215,7 @@ function comprobarMascaras(datoNuevo, posicion){
     //Si el dato no puede ser null
     if(datoNuevo == "" && !(posicion == 3 || posicion == 7 || posicion == 11 || posicion == 12))
     {
-      alert("El "+arrayNombresBBDD[posicion]+" no puede estar vac&#237;o");
+      alert("El "+arrayNombresBBDD[posicion]+" no puede estar vac0xC3o");
       return false;
     }
     //Máscara DNI
